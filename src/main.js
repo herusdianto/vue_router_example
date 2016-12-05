cd23d5acca34a114828338ae0f059385ce77832d
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2016-12-05 21:54:48 +0700

    add index route

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
-import App from './App'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const index = require('components/index.vue');
+
+const routes = [
+  { path: '/', component: index },
+]
+
+const router = new VueRouter({
+  routes // short for routes: routes
 })
+
+new Vue({
+  router
+}).$mount('#app')
