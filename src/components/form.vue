f459bd60926a8fd131bb8d0794dc82b3cf043973
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2016-12-05 22:03:15 +0700

    add form component

---
+++ src/components/form.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>{{message}}</h1>
+
+        <br>
+
+        <router-link :to="{ name: 'index' }">Index</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Form'
+            }
+        }
+    }
+</script>
