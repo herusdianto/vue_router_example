8aa754984e41e2c97dbafee92017dd07e633c8c3
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2016-12-05 22:09:00 +0700

    add user component

---
+++ src/components/user.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{message}}</h1>
+
+        <br>
+
+        <router-link :to="{ name: 'form' }">Form</router-link>
+        <router-link :to="{ name: 'index' }">Index</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'User ID: ' + this.$route.params.id
+            }
+        }
+    }
+</script>
